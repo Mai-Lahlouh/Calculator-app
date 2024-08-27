caa4ac554e2365b758f3166b61c7061834727ca0
May Lahlooh <nlahlouh09@gmail.com>
2024-08-27 15:54:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>{/*Theme */}
+        
+      </header>
+      <main>
+        <section>{/*Result screen */}</section>
+        <section>{/*calculator buttons */}</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
